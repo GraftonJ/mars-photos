edbe73a739c17f2af286304429c6643d31860528
Josh <grafton.josh@gmail.com>
2018-09-07 15:32:48 -0600

    Connected API to load images on page

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-      //Step 0: Ensure event listener works
-      console.log('DOMContentLoaded')
+//Step 0: Ensure event listener works
+console.log('DOMContentLoaded')
 
-      //Step 1: fetch some data from a server. axios? OMDBAPI
-      let url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2000&camera=fhaz&api_key=gAxMXQNXlPnc6I9neT00BCxN2vk5Cq0Qj5gLSSgf'
-      axios.get(url)
-        .then((response) => {
-            //Step 2: Log Data and see what you are getting back as a response
+//Step 1: fetch some data from a server. axios? OMDBAPI
+let inputDate = document.getElementById('date').value
+let earth_date = '2018-09-01'
+let button = document.getElementById('submit')
+let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${earth_date}&api_key=gAxMXQNXlPnc6I9neT00BCxN2vk5Cq0Qj5gLSSgf`
+button.addEventListener('click', function() {
+  axios.get(url)
+    .then((response) => {
+        //Step 2: Log Data and see what you are getting back as a response
+        console.log(response.data);
+        console.log(response.data.photos.length)
+        console.log(response.data.photos[0]['img_src'])
+        console.log(response.status)
+        //     //Step 3: Insert that data into your DOM, with some DOM creation/manipulation
+        let numPhotos = response.data.photos.length
+        for (i = 0; i < numPhotos; i++) {
+          const marsPhoto = response.data.photos[i]['img_src']
+          const div = document.getElementById('photo-div')
+          const DOM_img = document.createElement("img")
+          DOM_img.src = marsPhoto
+          div.appendChild(DOM_img)
+        }
+    })
+})
+})
 
-            console.log();
-            console.log(response.data);
-            console.log(response.data.photos[0]['img_src'])
-            console.log(response.status)
-            //     //Step 3: Insert that data into your DOM, with some DOM creation/manipulation
-
-            const marsPhoto = response.data.photos[0]['img_src']
-            const div = document.getElementById('photo-div')
-            const DOM_img = document.createElement("img")
-            DOM_img.src = marsPhoto
-            div.appendChild(DOM_img)
-              //
-              //     // ml3.innerText
-              //     // loop over movie results, creating <li>'s inside an <ol>
-              //
-              //     // create ol
-              //     let ol = document.createElement('ol')
-              //     let li
-              //
-              //     for (var i = 0; i < movies.length; i++) {
-              //       // create li
-              //       li = document.createElement('li')
-              //       li.innerText = `${movies[i].Title} (${movies[i].Year})`
-              //       ol.appendChild(li)
-              //     }
-              //
-              //     ml3.appendChild(ol)
-              //
-            })
-        })
+//
+//     // ml3.innerText
+//     // loop over movie results, creating <li>'s inside an <ol>
+//
+//     // create ol
+//     let ol = document.createElement('ol')
+//     let li
+//
+//     for (var i = 0; i < movies.length; i++) {
+//       // create li
+//       li = document.createElement('li')
+//       li.innerText = `${movies[i].Title} (${movies[i].Year})`
+//       ol.appendChild(li)
+//     }
+//
+//     ml3.appendChild(ol)
+//
