c9361e52fe9bc47a73e3a903cbbb1839260d0352
Josh <grafton.josh@gmail.com>
2018-09-08 12:22:04 -0600

    Each submit loads additional photos

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-//Step 0: Ensure event listener works
-console.log('DOMContentLoaded')
+  //Step 0: Ensure event listener works
+  console.log('DOMContentLoaded')
 
-//Step 1: fetch some data from a server. axios? OMDBAPI
-let inputDate = document.getElementById('date').value
-let earth_date = '2018-09-01'
-let button = document.getElementById('submit')
-let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${earth_date}&api_key=gAxMXQNXlPnc6I9neT00BCxN2vk5Cq0Qj5gLSSgf`
-button.addEventListener('click', function() {
-  axios.get(url)
-    .then((response) => {
+  let button = document.getElementById('submit')
+
+
+  button.addEventListener('click', function() {
+    //Step 1: fetch some data from a server. axios? OMDBAPI
+    if (document.getElementById('img')) {
+      div.removeChild(DOM_img)
+    }
+    else {
+    selectedDate = document.getElementById('date').value
+    let photoDate = selectedDate
+    console.log('pdate', photoDate);
+    let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${photoDate}&api_key=gAxMXQNXlPnc6I9neT00BCxN2vk5Cq0Qj5gLSSgf`
+    console.log(url);
+
+    axios.get(url)
+      .then((response) => {
         //Step 2: Log Data and see what you are getting back as a response
         console.log(response.data);
         console.log(response.data.photos.length)
         console.log(response.data.photos[0]['img_src'])
         console.log(response.status)
-        //     //Step 3: Insert that data into your DOM, with some DOM creation/manipulation
+        //Step 3: Insert that data into your DOM, with some DOM creation/manipulation
         let numPhotos = response.data.photos.length
         for (i = 0; i < numPhotos; i++) {
           const marsPhoto = response.data.photos[i]['img_src']
@@ -24,8 +33,9 @@ button.addEventListener('click', function() {
           DOM_img.src = marsPhoto
           div.appendChild(DOM_img)
         }
-    })
-})
+      })
+    }
+  })
 })
 
 //
